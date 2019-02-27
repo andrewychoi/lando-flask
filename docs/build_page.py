38472457fd80eb68ba9384bc6fa0662d7905557e
Andrew Choi <andrew@choi.com>
2019-02-27 00:22:31 -0500

    Add new build --all flag to templating engine

--- docs/build_page.py
+++ docs/build_page.py
@@ -1,11 +1,19 @@
 import argparse
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+TEMPLATE_DIR = 'templates/'
+EXCLUDE_TEMPLATES = ['base']
 
-def main(template_name: str):
+
+def render_template(template_name: str):
+    if template_name in EXCLUDE_TEMPLATES:
+        print("Trying to build excluded template {}, stopping".format(template_name))
+        return
+
     env = Environment(
-        loader=FileSystemLoader('templates/'),
+        loader=FileSystemLoader(TEMPLATE_DIR),
         autoescape=select_autoescape(['html', 'xml'])
     )
     print("Loading template for template {}...".format(template_name))
@@ -18,8 +26,20 @@ def main(template_name: str):
     print("Done")
 
 
+def render_all():
+    for file in os.listdir(TEMPLATE_DIR):
+        template_name = file.split(".html")[0]
+        print("Rendering {}".format(template_name))
+        render_template(template_name)
+
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('template_name', help='an integer for the accumulator')
+    parser = argparse.ArgumentParser(description='Build templates for Lando project')
+    parser.add_argument('template_name', nargs='?', help='Template to use')
+    parser.add_argument('--all', help='Build all templates', action='store_true')
     args = parser.parse_args()
-    main(args.template_name)
+    if args.all:
+        render_all()
+    else:
+        render_template(args.template_name)
